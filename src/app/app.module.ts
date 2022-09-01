06a0179cfe85f373c2348fa216f4396743a9fa24
unknown <tomas.d.zorzano@gmail.com>
2022-09-01 18:54:30 -0300

    Ejercicio 02

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './Component/bienvenido/bienvenido.component';
+import { LoginComponent } from './Component/login/login.component';
+import { ErrorComponent } from './Component/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule
